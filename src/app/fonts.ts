a6b203e54ca1d455f8275892d8a8bbe3396f2e63
jiwon0995 <jiwon0995@gmail.com>
2024-06-17 23:54:20 +0900

    font 설정 : next/font/local 폰트 추가, tailwind 변수 설정

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import localFont from 'next/font/local';
+
+export const pretendardRegular = localFont({
+  src: '../fonts/PretendardStd-Regular.woff2',
+  variable: '--font-pretendard-regular',
+});
+
+export const pretendardBold = localFont({
+  src: '../fonts/PretendardStd-Bold.woff2',
+  variable: '--font-pretendard-bold',
+});
